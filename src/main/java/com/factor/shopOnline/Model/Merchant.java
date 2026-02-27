dab23368017490551f1faf28560339d1437b77a4
Peace <peaaig123@gmail.com>
2026-02-27 02:01:29 +0100

    Role based Shopping Api

---
+++ src/main/java/com/factor/shopOnline/Model/Merchant.java
@@ -0,0 +1,27 @@
+package com.factor.shopOnline.Model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@Entity
+@NoArgsConstructor
+public class Merchant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Email
+    private String email;
+    private String password;
+
+    @NotNull(message = "Phone number is required")
+    private String phone;
+    private String username;
+    @Enumerated(EnumType.STRING)
+    private Roles roles;
+}
