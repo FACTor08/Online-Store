bd10885dc320eac56301c480de866b18274bb6ac
Peace <peaaig123@gmail.com>
2026-05-05 10:16:40 +0100

    Role based Shopping Api

--- src/main/java/com/factor/shopOnline/entity/Client.java
+++ src/main/java/com/factor/shopOnline/entity/Client.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Client {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Email
     private String email;
